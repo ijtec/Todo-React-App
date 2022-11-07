0bb732029fb8bf695dbe3b649d61ba71c4b6d34e
Okunade sherif <o.d.alsherif@gmail.com>
2022-11-07 21:47:01 +0100

    first commit

---
+++ src/Redux/Actions/actionTypes.js
@@ -0,0 +1,6 @@
+export const actionTypes = {
+    GET_BLOGS : 'GET_BLOGS',
+    POST_BLOG : 'POST_BLOG',
+    DELETE_BLOG : 'DELETE_BLOG',
+    LOADING : 'LOADING',
+}
---
+++ src/Redux/reducers/blogReducer.js
@@ -0,0 +1,21 @@
+import { actionTypes } from "../Actions/actionTypes";
+
+const initialState = {
+   blogs : [],
+   loading: false
+};
+
+export const blogReducer = (state = initialState ,{type,payload}) => {
+    switch (type) {
+        case actionTypes.GET_BLOGS:
+            return {...state, blogs:payload}
+        case actionTypes.LOADING:
+            return {...state, loading:true}
+        case actionTypes.POST_BLOG:
+            return {...state, blogs:payload}
+        case actionTypes.DELETE_BLOG:
+            return {...state, blogs:payload}
+        default:
+            return state
+    }
+}
