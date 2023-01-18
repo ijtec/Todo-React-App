cd4309c8324e4ccdf126aa9ed4b95a822dd4f44a
Imam Rasheedat Ahmad <71936704+rash1234567@users.noreply.github.com>
2023-01-18 22:06:55 +0000

    update

--- src/App.js
+++ src/App.js
@@ -22,22 +22,28 @@ useEffect(() => {
   return (
     <div className="App">
       {" "}
-      <div className="addUser">
+      <div className="addUsers mt-[5vh]  flex flex-col items-center justify-around lg:w-[400px] w-[300px] h-[250px]">
         <input
+          className="h-[40px] w-[80%] border-[1px] outline-[#9BE150] p-2 rounded-xl border-[#9BE150] border-solid"
           type="text"
-          placeholder="Name..."
+          placeholder="Input your Todo"
           onChange={(event) => {
             setName(event.target.value);
           }}
         />
-        <input
-          type="text"
-          placeholder="Username..."
-          onChange={(event) => {
-            setUsername(event.target.value);
-          }}
-        />
+        <div className="flex flex-col w-full h-[30%] items-center justify-between">
+          <h1 className="text-[20px] font-bold">Set Date</h1>
+          <input
+          className="h-[40px] w-[80%] border-[1px] outline-[#9BE150] p-2 rounded-xl border-[#9BE150] border-solid"
+            type="date"
+            placeholder="Username..."
+            onChange={(event) => {
+              setUsername(event.target.value);
+            }}
+          />
+        </div>
         <button
+         className="bg-[#9BE150] h-[15%] w-[40%] text-white rounded-xl font-semibold"
           onClick={() => {
             dispatch(
               addUser(
@@ -51,16 +57,17 @@ useEffect(() => {
           }}
         >
           {" "}
-          Add User
+          Add Todo
         </button>
       </div>
       <div className="displayUsers">
         {userList.map((user) => {
           return (
-            <div key={user.id}>
+            <div key={user.id} className='flex flex-col items-center'>
               <h1> {user.name}</h1>
               <h1> {user.username}</h1>
               <input
+                className="text-black p-2"
                 type="text"
                 placeholder="New Username..."
                 onChange={(event) => {
